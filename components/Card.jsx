3a61037f8f8d76d9facb39eddc361448d54b58a7
Kreceo <darrell-lane@hotmail.co.uk>
2022-01-01 13:08:38 +0000

    Refactor ( ESLint fixes )

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,24 +1,22 @@
 import Image from 'next/image';
-import * as S from '../src/styles/styles';
 import Link from 'next/link';
+import * as S from '../src/styles/styles';
 
 export default function Card() {
   return (
-    <Link href="/archive">
-      <a>
-        <S.BlogCard>
-          <S.BlogCardHeader>
-            <Image src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg" width="150" height="150" alt="" />
-          </S.BlogCardHeader>
-          <S.BlogCardBody>
-            <h4>The Majestic and Wonderful Bahamas</h4>
-            <p>
-              The best kept secret of The Bahamas is the country&apos;s sheer size and diversity.
-              With 16 major islands, The Bahamas is an unmatched destination
-            </p>
-          </S.BlogCardBody>
-        </S.BlogCard>
-      </a>
+    <Link href="/archive" passHref>
+      <S.BlogCard>
+        <S.BlogCardHeader>
+          <Image src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg" width="150" height="150" alt="" />
+        </S.BlogCardHeader>
+        <S.BlogCardBody>
+          <h4>The Majestic and Wonderful Bahamas</h4>
+          <p>
+            The best kept secret of The Bahamas is the country&apos;s sheer size and diversity.
+            With 16 major islands, The Bahamas is an unmatched destination
+          </p>
+        </S.BlogCardBody>
+      </S.BlogCard>
     </Link>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
